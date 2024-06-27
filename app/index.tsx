d87c82f31521c8b72797bc01d2dcd6fc887d7737
Mike <mikeheiser412@gmail.com>
2024-06-27 12:04:20 -0400

    form structuring

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,56 @@
-import { View, Text, StyleSheet } from 'react-native';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+    <Form>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Row>
+
+      <fieldset>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label as="legend" column sm={2}>
+            Radios
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Check
+              type="radio"
+              label="Male"
+              name="genderSelect"
+              id="genderMale"
+            />
+            <Form.Check
+              type="radio"
+              label="Female"
+              name="genderSelect"
+              id="genderFemale"
+            />
+          </Col>
+        </Form.Group>
+      </fieldset>
+
+      <Form.Group className="mb-3" id="noDnD">
+        <Form.Check type="checkbox" label="I Agree to Never Drink and Drive" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" id="tNC">
+        <Form.Check type="checkbox" label="I understand this is for education and entertainment purposes only and is not medical advice" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
